223149763471ad3c415b4edec6234993f4a916b6
Andrew McGraw <mcgraw@Andrews-MacBook-Pro.local>
2018-09-06 17:07:46 -0700

    Restructure towards object orientation

--- jbcmlscs/cmdline/chj_findsimilar_gui.py
+++ jbcmlscs/cmdline/chj_findsimilar_gui.py
@@ -42,6 +42,8 @@ from jbcmlscs.retrieval.JIndexJar import JIndexJar
 from jbcmlscs.similarity.JPattern import JPattern
 from jbcmlscs.similarity.JFindSimilar import JFindSimilar
 
+from jbcmlscs.gui.SimilarMethodsTab import SimilarMethodsTab
+
 featuresets = [ 'branch-conditions-v', 'branch-conditions-vmcfs',
                     'method-assignments-vmcfs', 'method-assignments-vmcfsi','signatures','sizes']
 
@@ -60,6 +62,55 @@ def timing():
     yield
     print('Completed in ' + str(time.time() - t0) + ' secs')
 
+class findSimilar():
+    def __init__(self, parent):
+        self.myParent = parent
+        self.notebook = ttk.Notebook(self.myParent)
+ 
+        self.tab1 = ttk.Frame(self.notebook) 
+        self.tab2 = SimilarMethodsTab(self.notebook)
+
+        self.configure_root()
+        self.tab2.configure()
+        self.build_tab1(results)
+        self.tab2.build(results)
+
+        self.tab2.label_frame.update_idletasks()
+        self.tab2.calculate_sizes()
+
+        self.notebook.add(self.tab1, text='term weights') 
+        self.notebook.add(self.tab2.tab, text='results')
+
+        self.notebook.pack(expand=1, fill='both')
+
+    def configure_root(self):
+        self.myParent.grid_rowconfigure(0, weight=1)
+        self.myParent.columnconfigure(0, weight=1)
+        self.myParent.title('Similarity results for ' + args.pattern)
+        self.myParent.geometry('1400x800')
+
+        style = ttk.Style()
+        style.theme_use('default')
+        style.configure('black.Horizontal.TProgressBar',background='blue')
+
+    def build_tab1(self, results):
+        row = 1
+        maxweight = sorted([ f['score'] for f in results['weights'] ],reverse=True)[0]
+        maxwidth = sorted([ len(f['feature']) for f in results['weights'] ], reverse=True)[0]
+        for s in sorted(featuresets):
+            if any([ f['featureset'] == s for f in results['weights'] ]):
+                lbl = Label(self.tab1, text=s,font=('Monaco',16),anchor=W,width=25,justify=LEFT)
+                lbl.grid(columnspan=2,row=row, sticky='w')
+                row += 1
+            for f in sorted(results['weights'],key=lambda f:f['score'],reverse=True):
+                if f['featureset'] == s:
+                    bar = Progressbar(self.tab1,length=200,style='black.Horizontal.TProgressbar')
+                    bar['value'] = f['score'] * (100.0 / maxweight)
+                    bar.grid(column=0,row=row)
+                    lbl = Label(self.tab1, text=f['feature'],
+                                    font=("Monaco", 12),anchor=W,width=maxwidth,justify=LEFT)
+                    lbl.grid(column=1,row=row)
+                    row += 1
 
 if __name__ == '__main__':
 
@@ -102,101 +153,7 @@ if __name__ == '__main__':
         results['methods'].append(m)
 
     window = Tk()
-    window.grid_rowconfigure(0, weight=1)
-    window.columnconfigure(0, weight=1)
-    window.title('Similarity results for ' + args.pattern)
-    window.geometry('1400x800')
-
-    style = ttk.Style()
-    style.theme_use('default')
-    style.configure('black.Horizontal.TProgressBar',background='blue')
-
-    notebook = ttk.Notebook(window) 
-    tab1 = ttk.Frame(notebook) 
-
-    tab2 = ttk.Frame(notebook)
-    tab2.grid(sticky='news')
-
-    tab2_cframe = ttk.Frame(tab2)
-    tab2_cframe.grid(row=0, column=0, pady=(5, 0), sticky='nw')
-    tab2_cframe.grid_rowconfigure(0, weight=1)
-    tab2_cframe.grid_columnconfigure(0, weight=1)
-    tab2_cframe.grid_propagate(False)
-
-    canvas = Canvas(tab2_cframe, bg="yellow")
-    canvas.grid(row=0, column=0, sticky="news")
-
-    tab2_scrollbar = ttk.Scrollbar(tab2_cframe, orient="vertical", command=canvas.yview)
-    tab2_scrollbar.grid(row=0, column=1, sticky='ns')
-    canvas.configure(yscrollcommand=tab2_scrollbar.set)
-
-    tab2_lframe = ttk.Frame(canvas)
-    canvas.create_window((0, 0), window=tab2_lframe, anchor='nw')
-
-    row = 1
-    maxweight = sorted([ f['score'] for f in results['weights'] ],reverse=True)[0]
-    maxwidth = sorted([ len(f['feature']) for f in results['weights'] ], reverse=True)[0]
-    for s in sorted(featuresets):
-        if any([ f['featureset'] == s for f in results['weights'] ]):
-            lbl = Label(tab1, text=s,font=('Monaco',16),anchor=W,width=25,justify=LEFT)
-            lbl.grid(columnspan=2,row=row, sticky='w')
-            row += 1
-        for f in sorted(results['weights'],key=lambda f:f['score'],reverse=True):
-            if f['featureset'] == s:
-                bar = Progressbar(tab1,length=200,style='black.Horizontal.TProgressbar')
-                bar['value'] = f['score'] * (100.0 / maxweight)
-                bar.grid(column=0,row=row)
-                lbl = Label(tab1, text=f['feature'],
-                                font=("Monaco", 12),anchor=W,width=maxwidth,justify=LEFT)
-                lbl.grid(column=1,row=row)
-                row += 1
-
-
-    seqlabels = []
-    progressbars = []
-    methodlabels = []
-    jarlabels = []
-
-    row = 1
-    maxfqmethodname = sorted([len(m['fqmethodname']) for m in results['methods'] ],reverse=True)[0]
-    maxjars = sorted([len(m['jarnames']) for m in results['methods'] ],reverse=True)[0]
-    for m in sorted(results['methods'],key=lambda m:m['score'],reverse=True):
-        seqlabel = Label(tab2_lframe,text=str(row),font=('Monaco',12),
-                             anchor=W,width=2,justify=RIGHT,padx=5)
-        seqlabels.append(seqlabel)
-        seqlabel.grid(column=0,row=row)
-        bar = Progressbar(tab2_lframe,length=200,style='black.Horizontal.TProgressbar')
-        bar['value'] = m['score'] * 100.0
-        progressbars.append(bar)
-        bar.grid(column=1,row=row)
-        methodlabel = Label(tab2_lframe,text=m['fqmethodname'],
-                                font=('Monaco',12),
-                                anchor=W,width=maxfqmethodname,
-                                justify=LEFT,padx=5)
-        jarlabel = Label(tab2_lframe,text=m['jarnames'],
-                             font=('Monaco',12),
-                             anchor=W,
-                             width=maxjars,justify=LEFT)
-        methodlabels.append(methodlabel)
-        jarlabels.append(jarlabel)
-        methodlabel.grid(column=2,row=row)
-        jarlabel.grid(column=3,row=row)
-        row += 1
-
-    tab2_lframe.update_idletasks()
-
-    labels_width = seqlabels[0].winfo_width() + progressbars[0].winfo_width() + methodlabels[0].winfo_width() + jarlabels[0].winfo_width()
-    numberrows = min(30, len(seqlabels))
-    first5rows_height = sum([seqlabels[i].winfo_height() for i in range(numberrows)])
-    tab2_cframe.config(width=labels_width + tab2_scrollbar.winfo_width(), height=first5rows_height)
-
-    canvas.config(scrollregion=canvas.bbox("all"))
-
-    notebook.add(tab1, text='term weights') 
-    notebook.add(tab2, text='results')
-
-    notebook.pack(expand=1, fill='both')
-
+    myapp = findSimilar(window)
     window.mainloop()
 
     # print(json.dumps(results,indent=4,sort_keys=True))          
---
+++ jbcmlscs/gui/SimilarMethodsTab.py
@@ -0,0 +1,100 @@
+# ------------------------------------------------------------------------------
+# Python API to access CodeHawk Java Analyzer analysis results
+# Author: Henny Sipma, Andrew McGraw
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2017 Kestrel Technology LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+from tkinter import *
+from tkinter.ttk import Progressbar
+from tkinter import ttk
+
+from contextlib import contextmanager
+
+class SimilarMethodsTab():
+
+    def __init__(self, parent):
+        self.myParent = parent
+        self.tab = ttk.Frame(parent)
+
+        self.tab.grid(sticky='news')
+        self.canvas_frame = ttk.Frame(self.tab)
+        self.canvas = Canvas(self.canvas_frame, bg="yellow")
+        self.scrollbar = ttk.Scrollbar(self.canvas_frame, orient="vertical", command=self.canvas.yview)
+        self.label_frame = ttk.Frame(self.canvas)
+
+        self.seqlabels = []
+        self.progressbars = []
+        self.methodlabels = []
+        self.jarlabels = []
+
+    def configure(self):
+        self.canvas_frame.grid(row=0, column=0, pady=(5, 0), sticky='nw')
+        self.canvas_frame.grid_rowconfigure(0, weight=1)
+        self.canvas_frame.grid_columnconfigure(0, weight=1)
+        self.canvas_frame.grid_propagate(False)
+
+        self.canvas.grid(row=0, column=0, sticky="news")
+
+        self.scrollbar.grid(row=0, column=1, sticky='ns')
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.canvas.create_window((0, 0), window=self.label_frame, anchor='nw')
+
+    def build(self, results):
+        row = 1
+        maxfqmethodname = sorted([len(m['fqmethodname']) for m in results['methods'] ],reverse=True)[0]
+        maxjars = sorted([len(m['jarnames']) for m in results['methods'] ],reverse=True)[0]
+        for m in sorted(results['methods'],key=lambda m:m['score'],reverse=True):
+            seqlabel = Label(self.label_frame,text=str(row),font=('Monaco',12),
+                                 anchor=W,width=2,justify=RIGHT,padx=5)
+            self.seqlabels.append(seqlabel)
+            seqlabel.grid(column=0,row=row)
+            bar = Progressbar(self.label_frame,length=200,style='black.Horizontal.TProgressbar')
+            bar['value'] = m['score'] * 100.0
+            self.progressbars.append(bar)
+            bar.grid(column=1,row=row)
+            methodlabel = Label(self.label_frame,text=m['fqmethodname'],
+                                font=('Monaco',12),
+                                anchor=W,width=maxfqmethodname,
+                                justify=LEFT,padx=5)
+            jarlabel = Label(self.label_frame,text=m['jarnames'],
+                                 font=('Monaco',12),
+                                 anchor=W,
+                                 width=maxjars,justify=LEFT)
+            self.methodlabels.append(methodlabel)
+            self.jarlabels.append(jarlabel)
+            methodlabel.grid(column=2,row=row)
+            jarlabel.grid(column=3,row=row)
+            row += 1
+
+    def calculate_sizes(self):
+        rowcount = min(30, len(self.seqlabels))
+        labels_width = self.seqlabels[0].winfo_width() + \
+            self.progressbars[0].winfo_width() + \
+            self.methodlabels[0].winfo_width() + \
+            self.jarlabels[0].winfo_width()
+        labels_height = sum([self.seqlabels[i].winfo_height() for i in range(rowcount)])
+
+        self.canvas_frame.config(width=labels_width + self.scrollbar.winfo_width(), height=labels_height)
+        self.canvas.config(scrollregion=self.canvas.bbox("all"))
