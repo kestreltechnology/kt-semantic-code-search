bd0a77f7d02fa752103ee9c50c96452121d2e91a
Henny Sipma <hennysipma@mac.com>
2018-10-06 18:50:47 -0700

    print information on features added

--- scs/jbc/cmdline/generate/chj_generate_features.py
+++ scs/jbc/cmdline/generate/chj_generate_features.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Python API to access CodeHawk Java Analyzer analysis results
+# Java byte code semantic code search
 # Author: Henny Sipma
 # ------------------------------------------------------------------------------
 # The MIT License (MIT)
@@ -41,7 +41,7 @@ processed, but the name of the jar is added to the list of files kept for that j
 
 Each jar in features-jarmanifest.json has a record that lists the indices of
 the class files present in the jar, organized by package. The class indices
-themselves are kept in the file <featurespath>/features-classmd5s.json.
+themselves are kept in the file <featurespath>/docindex/classmd5s.json.
 """
 
 import argparse
@@ -54,11 +54,50 @@ from scs.jbc.util.Config import Config
 
 from scs.jbc.index.FeaturesAdministrator import FeaturesAdministrator
 
+initial_counts = {
+    "files": 0 ,
+    "packages": 0,
+    "classes": 0
+    }
+
+final_counts = {
+    "files": 0,
+    "packages": 0,
+    "classes": 0
+    }
+
+def set_counts(admin,counts):
+    mf = admin.jarmanifest
+    counts['files'] = mf.file_count()
+    counts['packages'] = mf.package_count()
+    counts['classes'] = mf.class_count()
+
+def get_counts(counts):
+    return (str(counts['files']).rjust(10)
+                + str(counts['packages']).rjust(10)
+                + str(counts['classes']).rjust(10))
+
+def get_count_increments(c1,c2):
+    return (str(c2['files'] - c1['files']).rjust(10)
+                + str(c2['packages'] - c1['packages']).rjust(10)
+                + str(c2['classes'] - c1['classes']).rjust(10))
+
+def report():
+    lines = []
+    lines.append('             files  packages  classes')
+    lines.append('-' * 50)
+    lines.append('initial' + get_counts(initial_counts))
+    lines.append('final  ' + get_counts(final_counts))
+    lines.append('-' * 50)
+    lines.append('new    ' + get_count_increments(initial_counts,final_counts))
+    return '\n'.join(lines)
+
 @contextmanager
 def timing():
     t0 = time.time()
     yield
-    print('Completed all jarfiles in ' + str(time.time() - t0))
+    print('Completed all jarfiles in ' + str(time.time() - t0) + '\n')
+    print(report())
 
 def parse():
     parser = argparse.ArgumentParser()
@@ -69,7 +108,10 @@ def parse():
         
 if  __name__ == '__main__':
 
-    args = parse()
-    UF.create_features_docindex_directory(args.featurespath)
-    admin = FeaturesAdministrator(args.featurespath)
-    admin.generate_features(args.jarpath,Config().codehawk)
+    with timing():
+        args = parse()
+        UF.create_features_docindex_directory(args.featurespath)
+        admin = FeaturesAdministrator(args.featurespath)
+        set_counts(admin,initial_counts)
+        admin.generate_features(args.jarpath,Config().codehawk)
+        set_counts(admin,final_counts)
