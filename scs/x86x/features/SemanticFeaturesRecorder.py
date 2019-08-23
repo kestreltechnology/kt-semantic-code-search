a1e57d036784836cb1818fdaf6853b52625ecf03
Henny Sipma <hennysipma@mac.com>
2019-08-23 16:32:11 -0700

    tools to add semantic features

---
+++ scs/x86x/features/SemanticFeaturesRecorder.py
@@ -0,0 +1,55 @@
+# ------------------------------------------------------------------------------
+# Python API to access CodeHawk Binary Analyzer analysis results
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2019 Kestrel Technology LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+
+class SemanticFeaturesRecorder(object):
+
+    def __init__(self):
+        self.results = {}
+        self.featuresets = []
+
+    def reset(self):
+        self.results = {}
+
+    def add_term(self,featureset,term,n=1):
+        self.results.setdefault(featureset,{})
+        self.results[featureset].setdefault(term,0)
+        self.results[featureset][term] += n
+
+    def record_iocs(self,iocfeatures):
+        for fs in iocfeatures:
+            for term in iocfeatures[fs]:
+                self.add_term(fs,term,iocfeatures[fs][term])
+
+    def record(self,semanticfeatures):
+        features = semanticfeatures.features
+        for fs in features:
+            if fs == 'iocs':
+                self.record_iocs(features[fs])
+            else:
+                for term in features[fs]:
+                    self.add_term(fs,term,features[fs][term])
